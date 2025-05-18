f113e93cff73b5a2a318103272d714f9e51644c4
MomosDogDev <momosdog@example.com>
2025-05-18 19:18:29 +0900

    update

--- testIR.js
+++ testIR.js
@@ -6,8 +6,8 @@ const IR = new Gpio(23, {
   alert: true
 });
 
-ir.glitchFilter(10000); // 10ms 이상 신호만 감지
+IR.glitchFilter(10000); // 10ms 이상 신호만 감지
 
-ir.on('alert', (level, tick) => {
+IR.on('alert', (level, tick) => {
   console.log(`IR 센서 상태: ${level === 1 ? 'ON(감지)' : 'OFF(미감지)'}`);
 });
