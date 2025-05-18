ee462bb7f515739486b9ce4516736c623b126644
MomosDogDev <momosdog@example.com>
2025-05-18 19:43:51 +0900

    update

--- testDown.js
+++ testDown.js
@@ -6,8 +6,12 @@ const IR = new Gpio(23, {
   alert: true
 });
 
-IR.glitchFilter(10000);
+// IR.glitchFilter(10000);
 
-IR.on('alert', (level, tick) => {
-  console.log(`📡 IR 센서 상태: ${level === 1 ? '감지됨' : '없음'}`);
-});
+// IR.on('alert', (level, tick) => {
+//   console.log(`📡 IR 센서 상태: ${level === 1 ? '감지됨' : '없음'}`);
+// });
+setInterval(() => {
+  const value = IR.digitalRead();
+  console.log(`📡 IR 감지 상태: ${value === 1 ? '감지됨' : '없음'}`);
+}, 500);
